50ec1005c129777e4290b711ee317b652ab3eb35
xiaoguo88 <xiaoguo.pok@goalsmapper.com>
2022-02-17 17:07:18 +0800

    changed v1

--- src/controllers/Board.js
+++ src/controllers/Board.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
-import {Provider} from 'react-redux'
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
 import classNames from 'classnames'
-import {applyMiddleware, createStore} from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
-import uuidv1 from 'uuid/v1'
+import { v1 as uuidv1 } from 'uuid'
 import BoardContainer from './BoardContainer'
 import createTranslate from 'rt/helpers/createTranslate'
 import boardReducer from 'rt/reducers/BoardReducer'
@@ -11,31 +11,31 @@ import boardReducer from 'rt/reducers/BoardReducer'
 const middlewares = process.env.REDUX_LOGGING ? [logger] : []
 
 export default class Board extends Component {
-  constructor({id}) {
-    super()
-    this.store = this.getStore()
-    this.id = id || uuidv1()
-  }
+    constructor({ id }) {
+        super()
+        this.store = this.getStore()
+        this.id = id || uuidv1()
+    }
 
-  getStore = () => {
-    //When you create multiple boards, unique stores are created for isolation
-    return createStore(boardReducer, applyMiddleware(...middlewares))
-  }
+    getStore = () => {
+        //When you create multiple boards, unique stores are created for isolation
+        return createStore(boardReducer, applyMiddleware(...middlewares))
+    }
 
-  render() {
-    const {id, className, components} = this.props
-    const allClassNames = classNames('react-trello-board', className || '')
-    return (
-      <Provider store={this.store}>
-        <>
-          <components.GlobalStyle />
-          <BoardContainer
-            id={this.id}
-            {...this.props}
-            className={allClassNames}
-          />
-       </>
-      </Provider>
-    )
-  }
+    render() {
+        const { id, className, components } = this.props
+        const allClassNames = classNames('react-trello-board', className || '')
+        return (
+            <Provider store={this.store}>
+                <>
+                    <components.GlobalStyle />
+                    <BoardContainer
+                        id={this.id}
+                        {...this.props}
+                        className={allClassNames}
+                    />
+                </>
+            </Provider>
+        )
+    }
 }
